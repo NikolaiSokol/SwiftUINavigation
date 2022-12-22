4f9df1b97cd49be007b07954131deacd8bac1e98
Nikolai Sokol <nikolaisokol@icloud.com>
2022-12-22 09:46:49 +0300

    Initial Commit

---
+++ SwiftUINavigation/SwiftUINavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUINavigationApp.swift
+//  SwiftUINavigation
+//
+//  Created by Nikolai Sokol on 22.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUINavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
